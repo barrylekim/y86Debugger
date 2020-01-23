e76bc8ce8c8ec9fe296307ceb3fe958dc59b4064
bachlekim <lekimbach0912@gmail.com>
2020-01-23 12:18:21 -0800

    Added executeInstructions

--- instruction.c
+++ instruction.c
@@ -15,12 +15,10 @@ int memReadByte(machine_state_t *state,	uint64_t address, uint8_t *value) {
 
   /* THIS PART TO BE COMPLETED BY THE STUDENT */
   if (address > state->programSize) {
+    printf("memReadByte failed because address %"PRIx64" is larger than memory size\n", address);
     return 0;
   }
-  printf("value of address memReadByte %"PRIx64"\n\n",address);
-  printf("value of memory size %"PRIx64"\n\n",state->programSize);
   uint8_t byteAtAddress = state->programMap[address];
-  printf("value of byteAtAddress memReadByte %"PRIx64"\n\n",byteAtAddress);
   *value = byteAtAddress;
   return 1;
 }
@@ -33,6 +31,7 @@ int memReadQuadLE(machine_state_t *state, uint64_t address, uint64_t *value) {
 
   /* THIS PART TO BE COMPLETED BY THE STUDENT */
   if (address + 7 > state -> programSize){
+    printf("memReadQuadLE failed because address %"PRIx64" + 7 is larger than memory size\n", address);
     return 0;
   }
   uint64_t temp = 0x00000000000000;
@@ -40,8 +39,6 @@ int memReadQuadLE(machine_state_t *state, uint64_t address, uint64_t *value) {
         uint64_t pcincr = address + i;
         uint64_t valBytes = state->programMap[pcincr];
         temp = temp | valBytes << (8*i);
-        //printf("valBytes value  %"PRIx64"\n\n", valBytes);
-        printf("Little endian value after each concatenation %"PRIx64"\n\n", temp);
       };
   *value = temp;
   return 1;
@@ -53,7 +50,12 @@ int memReadQuadLE(machine_state_t *state, uint64_t address, uint64_t *value) {
 int memWriteByte(machine_state_t *state,  uint64_t address, uint8_t value) {
 
   /* THIS PART TO BE COMPLETED BY THE STUDENT */
-  return 0;
+  if (address > state->programSize){
+    printf("memWriteByte failed because address %"PRIx64" is larger than memory size\n", address);
+    return 0;
+  }
+  state->programMap[address] = value;
+  return 1;
 }
 
 /* Stores the specified quad-word (64-bit) value into memory, at the
@@ -63,7 +65,15 @@ int memWriteByte(machine_state_t *state,  uint64_t address, uint8_t value) {
 int memWriteQuadLE(machine_state_t *state, uint64_t address, uint64_t value) {
 
   /* THIS PART TO BE COMPLETED BY THE STUDENT */
-  return 0;
+  if (address + 7 > state->programSize){
+    printf("memWriteQuadLE failed because address %"PRIx64" is larger than memory size\n", address);
+    return 0;
+  }
+  for (int i = 7; i>= 0; i--){
+    uint8_t eachByte = (value >> (i*8)) & 0xFF;
+    memWriteByte(state, address + i,  eachByte);
+  }
+  return 1;
 }
 
 /* Fetches one instruction from memory, at the address specified by
@@ -83,12 +93,11 @@ int fetchInstruction(machine_state_t *state, y86_instruction_t *instr) {
     // Address bigger than memory size
     return 0;
   };
+  printf("Start of Fetch!!!!!!!!!!!!!!!!!!");
   printf("PC is now at %"PRIx64"\n", pc);
   printf("At that address the map points to  %"PRIx64"\n", instrAtPc);
   uint8_t icode = instrAtPc >> 4;
   uint8_t ifun = instrAtPc & 0x0F;
-  printf("icode value %"PRIx64"\n", icode);
-  printf("ifun value %"PRIx64"\n", ifun);
   switch (icode){
     case I_HALT: 
     {
@@ -144,8 +153,6 @@ int fetchInstruction(machine_state_t *state, y86_instruction_t *instr) {
       }
       instr->rA = rA;
       instr->rB = rB;
-      printf("rA value %"PRIx64"\n", instr->rA);
-      printf("rB value  %"PRIx64"\n", instr->rB);
       instr->valP = pc + 2;
       printf("next instruction address valP is %"PRIx64"\n", instr->valP);
       break;
@@ -214,8 +221,6 @@ int fetchInstruction(machine_state_t *state, y86_instruction_t *instr) {
       }
       instr->rA = rA;
       instr->rB = rB;
-      printf("rA value %"PRIx64"\n", instr->rA);
-      printf("rB value  %"PRIx64"\n", instr->rB);
       uint64_t * value;
       value = &instr->valC;
       if(!memReadQuadLE(state, pc + 2, value)){
@@ -273,12 +278,10 @@ int fetchInstruction(machine_state_t *state, y86_instruction_t *instr) {
       instr->ifun = ifun;
       instr->rA= rA;
       instr->valP=pc+2;
-      printf("rA value  %"PRIx64"\n", instr->rA);
       printf("next instruction address valP is %"PRIx64"\n", instr->valP);
       break;
     }
     default: 
-      printf("This shouldn't be here");
       printErrorInvalidInstruction(stdout,instr);
       instr->icode = I_INVALID;
       return 0;
@@ -296,61 +299,228 @@ int fetchInstruction(machine_state_t *state, y86_instruction_t *instr) {
 int executeInstruction(machine_state_t *state, y86_instruction_t *instr) {
 
   /* THIS PART TO BE COMPLETED BY THE STUDENT */
+  // Need to test for invalid instructions or memory access to invalid address
+  printf("Start of execution!!!!!!!!!!!!!!!!!!!!!!!\n");
   switch (instr->icode){
     case I_HALT:
     {
       // What to do with halt?
-      // What does it mean if address is beyond the limit of memory size for memReadQuadLE() helper function up there?
-      // Where is instr->valP updated??
+      return 1;
       break;
     }
     case I_NOP:
     {
+      state->programCounter = instr->valP;
       break;
     }
     case I_RRMVXX:
+    case I_JXX:
     {
+      int Cnd = 0;
+      uint8_t conditioncode = state->conditionCodes & 0x0F; //Condition code is now SF/ZF
+      switch (instr->ifun){
+        case C_NC:
+        {
+          Cnd = 1;
+          break;
+        }
+        case C_LE:
+        {
+          if((conditioncode == 0x2) || (conditioncode == 0x1)){ //10 or 01 because 11 will never happen (can raise sign and zero flag at the same time)
+            Cnd = 1;
+          }
+          break;
+        }
+        case C_L:
+        {
+          if((conditioncode == 0x3) || (conditioncode == 0x2)){ //11 or 10
+            Cnd = 1;
+          }
+          break;
+        }
+        case C_E:
+        {
+          if((conditioncode == 0x1) || (conditioncode == 0x3)){ //01 or 11
+            Cnd = 1;
+          }
+          break;
+        }
+        case C_NE:
+        {
+          if((conditioncode == 0x0) || (conditioncode == 0x2)){ //00 or 10
+            Cnd = 1;
+          }
+          break;
+        }
+        case C_GE:
+        {
+          if((conditioncode == 0x0) || (conditioncode == 0x1)){ //00 or 01
+            Cnd = 1;
+          }
+          break;
+        }
+        case C_G:
+        {
+          if(conditioncode == 0x0){ //00
+            Cnd = 1;
+          }
+          break;
+        }
+        default:
+          printf("Switching through cases in jump should not lead to this line. Ifun is wrong");
+          printErrorInvalidInstruction(stdout,instr);
+          instr->icode = I_INVALID;
+          return 0;
+          break;
+      }
+      if (instr->icode == I_JXX){
+        if (Cnd){
+          state->programCounter = instr->valC; //Let program counter jump to new address
+        } else {
+          state->programCounter = instr->valP; //Let program counter continue to next instruction
+        }
+      } else if (instr->icode == I_RRMVXX){
+        if (Cnd){
+          uint64_t valA = state->registerFile[instr->rA]; // valA is the content of register rA 
+          uint64_t valE = valA + 0;
+          state->registerFile[instr->rB] = valE;
+        }
+        state->programCounter = instr->valP;
+      }
       break;
     }
     case I_IRMOVQ:
     {
+      uint64_t valE = instr->valC + 0;
+      state->registerFile[instr->rB] = valE; // put number valE into the contents of register rB
+      state->programCounter = instr->valP; // update program counter
       break;
     }
     case I_RMMOVQ:
     {
+      uint64_t valA = state->registerFile[instr->rA]; // valA is the content of register rA in this case is what we want to put in memory
+      uint64_t valB = state->registerFile[instr->rB]; // valB is the content of register rB in this case is an address in memory
+      uint64_t valE = valB + instr->valC; // valE is the address in memory to put valA in. valC is the offset from valB
+      if(!memWriteQuadLE(state, valE, valA)){
+        printErrorInvalidMemoryLocation(stdout, instr, valE);
+        return 0;
+      };
+      state->programCounter = instr->valP;
       break;
     }
     case I_MRMOVQ:
     {
+      uint64_t valB = state->registerFile[instr->rB]; // valB is the content of register rB in this case is an address in memory
+      uint64_t valE = valB + instr->valC; // valE is the address in memory to read from and put into rA. valC is the offset from valB
+      // state->registerFile[instr->rA] = state->programMap[valE];
+      uint64_t tempVal;
+      if(!memReadQuadLE(state, valE, &tempVal)){
+        printErrorInvalidMemoryLocation(stdout, instr, valE);
+        return 0;
+      };
+      state->registerFile[instr->rA] = tempVal;
+      state->programCounter = instr->valP;
       break;
     }
     case I_OPQ:
     {
-      break;
-    }
-    case I_JXX:
-    {
+      int64_t valE;
+      uint64_t valA = state->registerFile[instr->rA]; // valA is the content of register rA 
+      uint64_t valB = state->registerFile[instr->rB]; // valB is the content of register rB 
+      if (instr->ifun == A_ADDQ){
+        valE = valA + valB;
+      }
+      if (instr->ifun == A_SUBQ){
+        valE = valB - valA;
+      }
+      if (instr->ifun == A_ANDQ){
+        valE = valA & valB;
+      }
+      if (instr->ifun == A_XORQ){
+        valE = valA ^ valB;
+      }
+      if (instr->ifun == A_MULQ){
+        valE = valA * valB;
+      }
+      if (instr->ifun == A_DIVQ){
+        valE = valB/valA;
+      }
+      if (instr->ifun == A_MODQ){
+        valE = valB % valA;
+      }
+
+      // Setting condition codes
+      if (valE == 0){
+        state->conditionCodes = state->conditionCodes | CC_ZERO_MASK;
+      } else {
+        state->conditionCodes = state->conditionCodes & CC_ZERO_MASK;
+      }
+
+      if (valE < 0){
+        state->conditionCodes = state->conditionCodes | CC_SIGN_MASK;
+      } else {
+        state->conditionCodes = state->conditionCodes & CC_SIGN_MASK;
+      }
+
+      state->registerFile[instr->rB] = valE;
+      state->programCounter = instr->valP;
       break;
     }
     case I_CALL:
     {
+      uint64_t valRsp = state->registerFile[4]; //Address that the stack pointer is pointing at 
+      uint64_t valE = valRsp - 8;
+      if(!memWriteQuadLE(state, valE, instr->valP)){
+        printErrorInvalidMemoryLocation(stdout, instr, valE);
+        return 0;
+      }; //Pushes the address of the next instruction after call onto the stack so we have sth to return to
+      state->registerFile[4] = valRsp - 8; //Moving the stack pointer down because there is something new on the stack
+      state->programCounter = instr->valC; // Program counter moves to the next instruction specified at the input address
       break;
     }
     case I_RET:
     {
+      uint64_t valRsp = state->registerFile[4]; //Decode
+      uint64_t tempVal;
+      if(!memReadQuadLE(state, valRsp, &tempVal)){
+        printErrorInvalidMemoryLocation(stdout, instr, valRsp);
+        return 0;
+      };
+      state->programCounter = tempVal; //Update program counter, moving it back to the instruction after the call
+      state->registerFile[4] = valRsp + 8; 
       break;
     }
     case I_PUSHQ:
     {
+      uint64_t valA = state->registerFile[instr->rA]; //Content of register rA to be pushed on the stack
+      uint64_t valRsp = state->registerFile[4]; //Address that the stack pointer is pointing at
+      if(!memWriteQuadLE(state, valRsp - 8, valA)){
+        printErrorInvalidMemoryLocation(stdout, instr, valRsp - 8);
+        return 0;
+      };
+      state->registerFile[4] = valRsp - 8; //Updating the stack pointer
+      state->programCounter = instr->valP;
       break;
     }
     case I_POPQ:
     {
+      uint64_t valRsp = state->registerFile[4]; //Address that the stack pointer is pointing at
+      uint64_t tempVal;
+      if(!memReadQuadLE(state, valRsp, &tempVal)){
+        printErrorInvalidMemoryLocation(stdout, instr, valRsp);
+        return 0;
+      }; //Putting the value read at the stack pointer back into rA
+      state->registerFile[instr->rA] = tempVal; //Putting the value read at the stack pointer back into rA
+      state->registerFile[4] = valRsp + 8; //Updating stack pointer
+      state->programCounter = instr->valP;
       break;
     }
     default:
+      printf("Invalid icode!!!");
+      instr->icode = I_INVALID;
+      printErrorInvalidInstruction(stdout, instr);
+      return 0;
       break;
   }
-
-  return 0;
+  return 1;
 }
